708cf9f39d18884e690198d30461aceda145933e
Ayodele <ayodele.olufemi@mnsu.edu>
2021-03-30 16:31:11 -0500

    completed

--- David/SecondExam/index.js
+++ David/SecondExam/index.js
@@ -31,15 +31,17 @@ function findResults(e) {
         mainText.textContent = `${data.results.length} results for "${artistName}"`;
         data.results.forEach(result => {
             const outerCard = document.createElement('div');
-            outerCard.style.width = "20%";
+            outerCard.style.width = "15%";
             outerCard.style.backgroundColor = "#a4cbd4";
             outerCard.style.margin = "10px";
             outerCard.style.textAlign = "center";
             outerCard.style.minHeight = "10vh";
             outerCard.style.padding = "5px";
+            outerCard.style.borderRadius = "0.5rem"
             outerCard.innerHTML = `
             <img src = "${result.artworkUrl100}">
-            <p>${result.artistName}</p>
+            <h3>${result.artistName}</h3>
+            <p>${result.collectionName}</p>
             `;
             searchResults.append(outerCard);
         });
