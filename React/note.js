5f39cfdf1e7d85291e4828b6a54c3fadb6ddb165
Patrick-FRE <patrick.lin@antra.com>
2021-04-05 16:25:56 -0400

    update React day 4 todolist

---
+++ React/note.js
@@ -0,0 +1,15 @@
+/**
+ * JSX: syntax sugar to help you call React.creatElement
+ * Virtual DOM:
+ * babel
+ * npm
+ * Js modulize: Import Export(ES 6) | common JS | SystemJS | AMD
+ * bundle
+ * bolierplate
+ * npx
+ * PWA
+ * tree shaking | Rollup.js
+ * sourceMap
+ * imutableJs
+ *
+ */
