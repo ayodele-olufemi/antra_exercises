e79bdc235b7e488ad4a8fe2919b1dd18bb9050e6
Ayodele <ayodele.olufemi@mnsu.edu>
2021-04-09 17:09:30 -0500

    done

--- thirdexam/src/App.js
+++ thirdexam/src/App.js
@@ -10,37 +10,30 @@ function App() {
 			id: 1,
 			title:
 				"Wisconsin Democratic election official: Don't water Trump's 'plant of baloney'",
-			body:
-				"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum odio accusantium numquam fugiat incidunt, ut nulla a recusandae eum quasi earum. Perferendis officia neque quam dicta, vel a quasi Quam harum dolore officia voluptates minus, tenetur asperiores id ullam repellat impedit vero est quod possimus aspernatur dolorem cupiditate cumque debitis laboriosam! Deserunt accusamus asperiores, nemo tenetur sequi illo repellat?",
+			body: `MADISON. Wis. - The Wisconsin Elections Commission late Wednesday after an hour long often-contentious debate agreed to issue an order on Thursday to recount ballots cast in Milwaukee and Dane counties as requested by President Donald Trump. Trump paid the $3 million required for the recount and issuing the order was expected to be a proforma move, but it instead resulted in nearly six hours of arguing. The partisan fighting before the recount has even started likely foreshadows the battle that lies ahead. "It's just remarkable the six of us in a civilized fashion can't agree to this stuff"; Democratic commissioner Mark Thomsen said hours into the debate.`,
 			color: "blue",
 		},
 		{
 			id: 2,
 			title:
 				"Current and former Trump officials quietly reach out to Bidem team",
-			body:
-				"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum odio accusantium numquam fugiat incidunt, ut nulla a recusandae eum quasi earum. Perferendis officia neque quam dicta, vel a quasi Quam harum dolore officia voluptates minus, tenetur asperiores id ullam repellat impedit vero est quod possimus aspernatur dolorem cupiditate cumque debitis laboriosam! Deserunt accusamus asperiores, nemo tenetur sequi illo repellat?",
+			body: `MADISON. Wis. - The Wisconsin Elections Commission late Wednesday after an hour long often-contentious debate agreed to issue an order on Thursday to recount ballots cast in Milwaukee and Dane counties as requested by President Donald Trump. Trump paid the $3 million required for the recount and issuing the order was expected to be a proforma move, but it instead resulted in nearly six hours of arguing. The partisan fighting before the recount has even started likely foreshadows the battle that lies ahead. "It's just remarkable the six of us in a civilized fashion can't agree to this stuff"; Democratic commissioner Mark Thomsen said hours into the debate.`,
 			color: "black",
 		},
 		{
 			id: 3,
 			title: "Republicans need to decide whether they believe in democracy",
-			body:
-				"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum odio accusantium numquam fugiat incidunt, ut nulla a recusandae eum quasi earum. Perferendis officia neque quam dicta, vel a quasi Quam harum dolore officia voluptates minus, tenetur asperiores id ullam repellat impedit vero est quod possimus aspernatur dolorem cupiditate cumque debitis laboriosam! Deserunt accusamus asperiores, nemo tenetur sequi illo repellat?",
+			body: `MADISON. Wis. - The Wisconsin Elections Commission late Wednesday after an hour long often-contentious debate agreed to issue an order on Thursday to recount ballots cast in Milwaukee and Dane counties as requested by President Donald Trump. Trump paid the $3 million required for the recount and issuing the order was expected to be a proforma move, but it instead resulted in nearly six hours of arguing. The partisan fighting before the recount has even started likely foreshadows the battle that lies ahead. "It's just remarkable the six of us in a civilized fashion can't agree to this stuff"; Democratic commissioner Mark Thomsen said hours into the debate.`,
 			color: "red",
 		},
 		{
 			id: 4,
 			title: "Republicans need to decide whether they believe in democracy",
-			body:
-				"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum odio accusantium numquam fugiat incidunt, ut nulla a recusandae eum quasi earum. Perferendis officia neque quam dicta, vel a quasi Quam harum dolore officia voluptates minus, tenetur asperiores id ullam repellat impedit vero est quod possimus aspernatur dolorem cupiditate cumque debitis laboriosam! Deserunt accusamus asperiores, nemo tenetur sequi illo repellat?",
+			body: `MADISON. Wis. - The Wisconsin Elections Commission late Wednesday after an hour long often-contentious debate agreed to issue an order on Thursday to recount ballots cast in Milwaukee and Dane counties as requested by President Donald Trump. Trump paid the $3 million required for the recount and issuing the order was expected to be a proforma move, but it instead resulted in nearly six hours of arguing. The partisan fighting before the recount has even started likely foreshadows the battle that lies ahead. "It's just remarkable the six of us in a civilized fashion can't agree to this stuff"; Democratic commissioner Mark Thomsen said hours into the debate.`,
 			color: "green",
 		},
 	];
 
-	// const clickHandler = (e) => {
-	//   e.target.
-	// };
 	return (
 		<div className="App">
 			<div className="newsHeader">
