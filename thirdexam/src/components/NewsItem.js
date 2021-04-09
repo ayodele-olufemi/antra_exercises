0cfad1684d22202bdc72f23f0abdea54011e872c
Ayodele <ayodele.olufemi@mnsu.edu>
2021-04-09 16:05:23 -0500

    add blur

--- thirdexam/src/components/NewsItem.js
+++ thirdexam/src/components/NewsItem.js
@@ -17,13 +17,24 @@ const NewsItem = ({ news }) => {
 		e.view.document.querySelector("h1").style.color = news.color;
 		console.log(e.target.className);
 		e.target.parentNode.style.border = `${news.color} 2px solid`;
+		e.target.parentNode.style.boxShadow = `3px 3px 0 1px ${news.color}`;
+	};
+
+	const blurHandler = (e) => {
+		e.target.parentNode.style.border = "none";
+		e.target.parentNode.style.boxShadow = "none";
 	};
 
 	return (
 		<div className="card" id={news.id}>
 			<h2>{news.title}</h2>
 			<p>{news.body}</p>
-			<button className={news.id} style={buttonStyle} onClick={clickHandler}>
+			<button
+				className={news.id}
+				style={buttonStyle}
+				onClick={clickHandler}
+				onBlur={blurHandler}
+			>
 				Set {news.color}
 			</button>
 		</div>
